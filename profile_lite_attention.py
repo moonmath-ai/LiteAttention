5d30f1a1fd6cf20577ba117002d2efd8cc691ece
shmil <dorshmil@gmail.com>
2025-10-19 10:28:05 +0000

    improvment the profile python file

--- profile_lite_attention.py
+++ profile_lite_attention.py
@@ -5,18 +5,18 @@ torch.manual_seed(0)
 torch.cuda.manual_seed(0)
 
 def init_qkv(head_dim):
-    q = torch.randn(1, 5000, 4, head_dim, device="cuda", dtype=torch.bfloat16)
-    k = torch.randn(1, 5000, 4, head_dim, device="cuda", dtype=torch.bfloat16)
-    v = torch.randn(1, 5000, 4, head_dim, device="cuda", dtype=torch.bfloat16)
+    q = torch.randn(1, 10000, 4, head_dim, device="cuda", dtype=torch.bfloat16)
+    k = torch.randn(1, 10000, 4, head_dim, device="cuda", dtype=torch.bfloat16)
+    v = torch.randn(1, 10000, 4, head_dim, device="cuda", dtype=torch.bfloat16)
     return q, k, v
 
 for head_dim in [32, 64, 96, 128, 192, 256]:
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print(f"head_dim: {head_dim}")
-    attn = LiteAttention()
-    attn.threshold = float(2)
+    attn = LiteAttention(enable_skipping=False)
+    attn.threshold = float(0.2)
     for i in range(2):
         q, k, v = init_qkv(head_dim)
         output = attn(q, k, v)
     torch.cuda.synchronize()
-    print(attn._skip_list.shape)
+    # print(attn._skip_list.shape)
