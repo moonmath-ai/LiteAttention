b407681e2e600f02feaa06f1dd5407280ac6797d
shmil <dorshmil@gmail.com>
2025-10-27 07:25:55 +0000

    only one warpgroup reset skip_tests

--- hopper/_internal/cpp/softmax.h
+++ hopper/_internal/cpp/softmax.h
@@ -157,7 +157,10 @@ namespace flash
             {
                 flash::template reduce_max</*zero_init=*/true>(scores, row_max);
                 cute::fill(scores_scale, 1.f);
-                if (is_warp_leader){ skip_tests[warp_idx_in_warpgroup] = false; }
+
+                if constexpr (!softmax_cond_assign){
+                    if (is_warp_leader){ skip_tests[warp_idx_in_warpgroup] = false; }
+                }
             }
             else
             {
