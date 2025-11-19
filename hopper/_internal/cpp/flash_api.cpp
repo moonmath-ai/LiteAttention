326d977e1db4f15288849ae4ec8e089cad0af756
Tarik Sarac <tariksarac120@gmail.com>
2025-11-19 13:00:09 +0300

    Updated kernel validation check for must do list dimension

--- hopper/_internal/cpp/flash_api.cpp
+++ hopper/_internal/cpp/flash_api.cpp
@@ -962,7 +962,7 @@ mha_fwd(at::Tensor q,   // (b, s_q, h, d) or (total_q, h, d) if there is cu_seql
     if (attn_must_do_list_.has_value()) {
         auto qk_skip_mask_tensor = attn_must_do_list_.value();
         TORCH_CHECK(qk_skip_mask_tensor.dtype() == torch::kInt16, "attn_must_do_list must be int16 tensor");
-        TORCH_CHECK(qk_skip_mask_tensor.dim() == 4, "attn_must_do_list must be 4D tensor with shape [batch, heads, q_blocks, k_blocks]");
+        TORCH_CHECK(qk_skip_mask_tensor.dim() == 1, "attn_must_do_list must be 1D tensor with shape [length]");
         TORCH_CHECK(qk_skip_mask_tensor.is_contiguous(), "attn_must_do_list must be contiguous");
         
         int16_t* data_ptr = static_cast<int16_t*>(qk_skip_mask_tensor.data_ptr());
