6efe3a8bce7e64b3bdabcbd2b5286540a9439dc7
dorshmil <dorshmil@gmail.com>
2026-01-12 09:49:50 +0000

    reverted back to fastest config

--- hopper/_internal/cpp/softmax.h
+++ hopper/_internal/cpp/softmax.h
@@ -496,7 +496,7 @@ namespace flash
             // consider: scores are Int's when Is_INT8 is enabled, so we need to pass the dequan_s instead of scale_apply_exp2
             //           and also we need to define a new scores tensor for float type (we dequantize and take the exp2 inside this function)
             // flash::template scale_apply_exp2</*Scale_max=*/true, Check_inf, Max_offset>(scores, row_max, softmax_scale_log2);
-            flash::template scale_apply_exp2_dequantize</*Scale_max=*/true, Check_inf, Max_offset, false>(scores, row_max, scores_float, dequan_s);
+            flash::template scale_apply_exp2_dequantize</*Scale_max=*/true, Check_inf, Max_offset>(scores, row_max, scores_float, dequan_s);
 
             // consider: here we should reduce_sum with the values of the float exp2 scores (which we need to create a float tensor for when Is_INT8 is enabled)
             // We don't do the reduce across threads here since we don't need to use the row_sum.
