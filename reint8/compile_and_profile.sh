2137e66f46b7c8d493d6557c88c3bbb59c52577c
dorshmil <dorshmil@gmail.com>
2026-01-12 08:56:31 +0000

    moved some scripts to reint8 folder

---
+++ reint8/compile_and_profile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Compile step
+cd hopper && python setup.py install > ../reint8/compile.logs && python ../reint8/demangle_logs.py && cd ..
+
+# python test_lite_attention.py
+# python ./reint8/events_bf16_fp8_int8_FA3_LA_profile.py
+
+# Profile step
+# ncu -o profile/bf16_fp8_int8_FA3_LA_profile%i --kernel-name device_kernel --launch-skip 4 --set full python reint8/bf16_fp8_int8_FA3_LA_profile.py
