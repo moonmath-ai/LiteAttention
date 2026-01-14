a16e296fbc382d9bebee4c034abf87a79c3d0328
shmil <dorshmil@gmail.com>
2026-01-14 07:44:09 +0000

    updated the compile and profile script to use custom nsight compute section files

--- reint8/compile_and_profile.sh
+++ reint8/compile_and_profile.sh
@@ -7,4 +7,5 @@ cd hopper && python setup.py install > ../reint8/compile.logs && python ../reint
 # python ./reint8/events_bf16_fp8_int8_FA3_LA_profile.py
 
 # Profile step
+ncu -o profile/bf16_fp8_int8_FA3_LA_profile%i --kernel-name device_kernel --launch-skip 4 --set full --section-folder-recursive ../Sections python reint8/bf16_fp8_int8_FA3_LA_profile.py
 # ncu -o profile/bf16_fp8_int8_FA3_LA_profile%i --kernel-name device_kernel --launch-skip 4 --set full python reint8/bf16_fp8_int8_FA3_LA_profile.py
