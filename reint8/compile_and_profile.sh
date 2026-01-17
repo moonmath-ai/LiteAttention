aac9d9ec84ad40447026b65b69b0a370d26e8a39
shmil <dorshmil@gmail.com>
2026-01-17 08:29:39 +0000

    bug fix - failing to find compile_logs.txt in demangle_logs.py

--- reint8/compile_and_profile.sh
+++ reint8/compile_and_profile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Compile step
-cd hopper && python setup.py install > ../reint8/compile.logs && python ../reint8/demangle_logs.py && cd ..
+cd hopper && python setup.py install > ../reint8/compile.logs && cd ../reint8 && python demangle_logs.py && cd ..
 
 # python test_lite_attention.py
-# python ./reint8/events_bf16_fp8_int8_FA3_LA_profile.py
+python reint8/events_bf16_fp8_int8_FA3_LA_profile.py
 
 # Profile step
 ncu -o profile/bf16_fp8_int8_FA3_LA_profile%i --kernel-name device_kernel --launch-skip 4 --set full --section-folder-recursive ../Sections python reint8/bf16_fp8_int8_FA3_LA_profile.py
