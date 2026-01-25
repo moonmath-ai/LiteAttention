9d46b0dec05af3d86425e898bbc97544365321e1
tsvika <tsvika@moonmath.ai>
2026-01-25 12:09:24 +0000

    fix: threshold exception when doing t2v

--- hopper/lite_attention.py
+++ hopper/lite_attention.py
@@ -814,7 +814,7 @@ class LiteAttention:
         Changing the threshold does not reset the skip list state. The new threshold
         will be applied starting from the next forward pass.
         """
-        if threshold >= 0 and os.getenv("LITE_ATTENTION_DEBUG", "FALSE") == "FALSE":
+        if torch.any(threshold >= 0) and os.getenv("LITE_ATTENTION_DEBUG", "FALSE") == "FALSE":
             raise ValueError("threshold must be negative when debug mode is not enabled")
 
         self.threshold = threshold
